fd1dd2810db2c7b0b9b8d3aeeab5d071600e868e
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-01-12 17:13:49 +0300

    Финализация

--- Задание 14/Program.cs
+++ Задание 14/Program.cs
@@ -10,6 +10,59 @@ namespace Задание_14
     {
         static void Main(string[] args)
         {
+            Animal cat = new Cat();
+            Animal dog = new Dog();
+            cat.ShowInfo();
+            dog.ShowInfo(); 
+            Console.ReadKey();
+        }
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+            public abstract void Say();
+            public void ShowInfo()
+            {
+                Console.WriteLine($"Название животного: {Name}");
+                Say();
+            }
+        }
+        class Cat : Animal
+        {
+            string name = "Кот";
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Мяу!");
+            }
+        }
+        class Dog : Animal
+        {
+            string name = "Собака";
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Гав!");
+            }
         }
     }
 }
